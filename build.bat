314a93884d215e11467522b4f157fe639d5c15c7
badestrand <badestrand@31957799-4632-4615-9793-72db6d48bcac>
2009-12-21 20:38:35 +0000

    Run the build.bat file with qemufloppy or qemuimage as parameter to start qemu.
    git-svn-id: svn://svn.code.sf.net/p/prettyos/code/trunk/Source@51 31957799-4632-4615-9793-72db6d48bcac

--- build.bat
+++ build.bat
@@ -7,8 +7,11 @@ IF [%1]==[] GOTO Continue
 	IF [%1]==[bochs] (
 		cmd /c tools\bochs.bxrc
 	)
-	IF [%1]==[qemu] (
-		echo ""
+	IF [%1]==[qemufloppy] (
+		qemu-system-x86_64.exe -usbdevice mouse -fda \\.\a: -boot a -localtime
+	)
+	IF [%1]==[qemuimage] (
+		qemu-system-x86_64.exe -usbdevice mouse -fda FloppyImage.bin -boot a -localtime
 	)
 	IF [%1]==[disc] (
 		tools\dd if=stage1_bootloader/boot.bin of=\\.\A: bs=512 count=1 --progress
