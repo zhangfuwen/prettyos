1e105787152776ef94636bd8398cc2a1a83f6af7
ehenkes <ehenkes@31957799-4632-4615-9793-72db6d48bcac>
2010-02-22 19:41:41 +0000

    - Zwischenschritt in RTL8139.c
    git-svn-id: svn://svn.code.sf.net/p/prettyos/code/trunk/Source@127 31957799-4632-4615-9793-72db6d48bcac

--- kernel/rtl8139.c
+++ kernel/rtl8139.c
@@ -6,6 +6,7 @@
 #include "os.h"
 #include "pci.h"
 #include "paging.h"
+#include "ipTcpStack.h"
 
 extern pciDev_t pciDev_Array[50];
 
@@ -16,6 +17,7 @@ extern uint32_t BaseAddressRTL8139;      ///TEST for network card
 
 /// this is the handler for an IRQ interrupt of our Network Card
 // only here for tests --> TODO: own module
+/*
 void rtl8139_handler(struct regs* r)
 {
 	/// TODO: ring buffer, we get always the first received data!
@@ -84,6 +86,85 @@ void rtl8139_handler(struct regs* r)
 
     settextcolor(15,0);
 }
+*/
+
+// proposal of tty:
+void rtl8139_handler(struct regs* r)
+{
+	/// TODO: ring buffer, we get always the first received data!
+
+	// read bytes 003Eh bis 003Fh, Interrupt Status Register
+    uint16_t val = *((uint16_t*)( BaseAddressRTL8139_MMIO + 0x3E ));
+
+    char str[80];
+    strcpy(str,"");
+
+    if((val & 0x1) == 0x1)
+    {
+        strcpy(str,"Receive OK,");
+    }
+    if((val & 0x4) == 0x4)
+    {
+        strcpy(str,"Transfer OK,");
+    }
+    settextcolor(3,0);
+    printformat("\n--------------------------------------------------------------------------------");
+    settextcolor(14,0);
+    printformat("\nRTL8139 IRQ: %y, %s  ", val,str);
+    settextcolor(3,0);
+
+    // reset interrupts bei writing 1 to the bits of offset 003Eh bis 003Fh, Interrupt Status Register
+	*((uint16_t*)( BaseAddressRTL8139_MMIO + 0x3E )) = val;
+
+	strcat(str,"   Receiving Buffer content:\n");
+	printformat(str);
+
+    int32_t length, ethernetType, i;
+    length = network_buffer[3]*0x100 + network_buffer[2]; // Little Endian
+    if (length>300) length = 300;
+    ethernetType = network_buffer[16]*0x100 + network_buffer[17]; // Big Endian
+
+    // output receiving buffer
+    settextcolor(13,0);
+    printformat("Flags: ");
+    settextcolor(3,0);
+    for(i=0;i<2;i++) {printformat("%y ",network_buffer[i]);}
+
+	// settextcolor(13,0); printformat("\tLength: "); settextcolor(3,0);
+    // for(i=2;i<4;i++) {printformat("%y ",network_buffer[i]);}
+    uint32_t paket_length = (network_buffer[3] << 8) | network_buffer[2];
+
+    settextcolor(13,0);
+	printformat("\tLength: ");	settextcolor(3,0);	printformat("%d", paket_length);
+
+	settextcolor(13,0); printformat("\nMAC Receiver: "); settextcolor(3,0);
+    for(i=4;i<10;i++) {printformat("%y ",network_buffer[i]);}
+
+    settextcolor(13,0); printformat("MAC Transmitter: "); settextcolor(3,0);
+    for(i=10;i<16;i++) {printformat("%y ",network_buffer[i]);}
+
+    settextcolor(13,0);
+    printformat("\nEthernet: ");
+    settextcolor(3,0);
+    if(ethernetType<=0x05DC){  printformat("type 1, "); }
+    else                    {  printformat("type 2, "); }
+
+    settextcolor(13,0);
+    if(ethernetType<=0x05DC){  printformat("Length: "); }
+    else                    {  printformat("Type: ");   }
+    settextcolor(3,0);
+    for(i=16;i<18;i++) {printformat("%y ",network_buffer[i]);}
+
+    printformat("\n");
+    for(i=18;i<=length;i++) {printformat("%y ",network_buffer[i]);}
+    printformat("\n--------------------------------------------------------------------------------\n");
+
+    settextcolor(15,0);
+
+	// call to the IP-TCP Stack
+	ipTcpStack_recv((void*)(&(network_buffer[4])), paket_length);
+}
+
 
 void install_RTL8139(uint32_t number)
 {
