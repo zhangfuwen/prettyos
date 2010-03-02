e58d71025d6e17b99651fd300b4fdaff19478b8e
ehenkes <ehenkes@31957799-4632-4615-9793-72db6d48bcac>
2010-03-02 19:38:16 +0000

    - timer.c Korrektur
    git-svn-id: svn://svn.code.sf.net/p/prettyos/code/trunk/Source@164 31957799-4632-4615-9793-72db6d48bcac

--- kernel/timer.c
+++ kernel/timer.c
@@ -63,7 +63,8 @@ void systemTimer_setFrequency( uint32_t freq )
     uint32_t divisor = 1193180 / systemfrequency; //divisor must fit into 16 bits
                                                   // PIT (programable interrupt timer)
     // Send the command byte
-    outportb(0x43, 0x36);
+    // outportb(0x43, 0x36); // 0x36 -> Mode 3 : Square Wave Generator
+       outportb(0x43, 0x34); // 0x34 -> Mode 2 : Rate Generator // thx to +gjm+
 
     // Send divisor
     outportb(0x40, (uint8_t)(  divisor     & 0xFF )); // low  byte
