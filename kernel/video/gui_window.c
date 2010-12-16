0fe0c26098f3b329e8cd57ccd01ad1897b5716f4
pkeus <pkeus@31957799-4632-4615-9793-72db6d48bcac>
2010-12-16 16:11:25 +0000

    Version 0.0.2.10:
    - Fenster nun wieder angezeigt
    
    git-svn-id: svn://svn.code.sf.net/p/prettyos/code/trunk/Source@846 31957799-4632-4615-9793-72db6d48bcac

--- kernel/video/gui_window.c
+++ kernel/video/gui_window.c
@@ -6,6 +6,7 @@
 #include "gui_window.h"
 #include "vbe.h"
 #include "kheap.h"
+#include "util.h"
 
 extern ModeInfoBlock_t mib;
 
@@ -74,7 +75,20 @@ void CreateWindow(char* windowname, uint16_t x, uint16_t y, uint16_t width, uint
 
     window->data = malloc((width*height)*(mib.BitsPerPixel/8), 0, "Window buffer"); // Creates buffer for window
 
-    reDrawWindow(window->id);
+    // Fill
+    vbe_drawRectFilled(window->x, window->y+20, window->x+window->width, window->y+window->height, WINDOW_COLOUR_BACKGROUND);
+
+    // Topbar
+    vbe_drawRectFilled(window->x, window->y, window->x+window->width, window->y+20, WINDOW_COLOUR_TOPBAR);
+
+    // Border
+    vbe_drawRect(window->x, window->y, window->x+window->width, window->y+window->height, WINDOW_COLOUR_BORDER);
+
+    // Title
+    vbe_drawString(window->name, window->x+2, window->y+2);
+
+    // TODO:
+    //reDrawWindow(window->id);
 
     // And set window focus
     current_window = window;
