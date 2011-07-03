41a9ab8262d20ce076906901e52d1d18191e9486
ehenkes <ehenkes@31957799-4632-4615-9793-72db6d48bcac>
2011-07-03 14:24:46 +0000

    irc.c hinzugefügt, experimentalstatus, aber es gelang damit der eintritt in den euirc chat #PrettyOS
    git-svn-id: svn://svn.code.sf.net/p/prettyos/code/trunk/Source@1020 31957799-4632-4615-9793-72db6d48bcac

--- kernel/ckernel.c
+++ kernel/ckernel.c
@@ -26,7 +26,7 @@
 #include "netprotocol/tcp.h"
 
 
-const char* const version = "0.0.2.178 - Rev: 1019";
+const char* const version = "0.0.2.178 - Rev: 1020";
 
 // .bss
 extern uintptr_t _bss_start; // linker script
