625c3efd5a5df53e79a2bdbec5a0247b959683bb
ehenkes <ehenkes@31957799-4632-4615-9793-72db6d48bcac>
2011-07-29 14:36:22 +0000

    browser.elf verallgemeinert: Man kann nun eine IP-Adresse (Zahlenwerte) eingeben.
    git-svn-id: svn://svn.code.sf.net/p/prettyos/code/trunk/Source@1099 31957799-4632-4615-9793-72db6d48bcac

--- user/other_userprogs/browser.c
+++ user/other_userprogs/browser.c
@@ -12,7 +12,15 @@ int main()
     printLine("--------------------------------------------------------------------------------", 4, 0x0B);
 
     iSetCursor(0, 7);
-    IP_t IP = {.IP = {82,100,220,68}}; // homepage ehenkes, HTTP
+    IP_t IP;
+    char strIP[4][10];
+    for (uint8_t i = 0; i<4; i++)
+    {        
+        printf("IP[%u]: ",i);
+        IP.IP[i] = atoi(gets(strIP[i]));
+    }
+    printf("\n%u.%u.%u.%u\n", IP.IP[0],IP.IP[1],IP.IP[2],IP.IP[3]);
+    
     uint32_t connection = tcp_connect(IP, 80);
     printf("\nConnected (ID = %u). Wait until connection is established... ", connection);
 
@@ -29,7 +37,19 @@ int main()
             case EVENT_TCP_CONNECTED:
             {
                 printf("ESTABLISHED.\n");
-                char* pStr = "GET /OS_Dev/PrettyOS.htm HTTP/1.1\r\nHost: www.henkessoft.de\r\nConnection: close\r\n\r\n";
+                
+                char pStr[200];
+                strcat(pStr,"GET / HTTP/1.1\r\nHost: ");
+                for (uint8_t i=0; i<3; i++)
+                {
+                    strcat(pStr,strIP[i]);
+                    strcat(pStr, ".");
+                }
+                strcat(pStr,strIP[3]);
+                strcat(pStr,"\r\nConnection: close\r\n\r\n");
+                textColor(0x0A);
+                printf("%s",pStr);
+                textColor(0x0F);
                 tcp_send(connection, pStr, strlen(pStr));
                 break;
             }
