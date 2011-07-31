708174641b3713ea02bb9ba69a91588781885e62
cuervo19 <cuervo19@31957799-4632-4615-9793-72db6d48bcac>
2011-07-31 17:04:14 +0000

    Rev. 1116:
    * rctl.c: Remote Control als User-Programm hinzugefügt
    
    git-svn-id: svn://svn.code.sf.net/p/prettyos/code/trunk/Source@1116 31957799-4632-4615-9793-72db6d48bcac

--- kernel/ckernel.c
+++ kernel/ckernel.c
@@ -37,7 +37,7 @@
 #include "netprotocol/tcp.h"    // passive opened connection (LISTEN)
 
 
-const char* const version = "0.0.2.267 - Rev: 1115";
+const char* const version = "0.0.2.268 - Rev: 1116";
 
 // .bss
 extern uintptr_t _bss_start; // linker script
