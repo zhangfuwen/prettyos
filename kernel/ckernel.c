6cc04eef98c82b5363515b5eb8b53f49ace9d807
neueruser <neueruser@31957799-4632-4615-9793-72db6d48bcac>
2011-08-12 14:38:35 +0000

    ftp.c: Funktionen hinzugefügt
    git-svn-id: svn://svn.code.sf.net/p/prettyos/code/trunk/Source@1163 31957799-4632-4615-9793-72db6d48bcac

--- kernel/ckernel.c
+++ kernel/ckernel.c
@@ -38,7 +38,7 @@
 #include "netprotocol/tcp.h"    // passive opened connection (LISTEN)
 
 
-const char* const version = "0.0.2.307 - Rev: 1162";
+const char* const version = "0.0.2.307 - Rev: 1163";
 
 // .bss
 extern uintptr_t _bss_start; // linker script
--- user/other_userprogs/ftp.c
+++ user/other_userprogs/ftp.c
@@ -2,21 +2,32 @@
 #include "stdlib.h"
 #include "stdio.h"
 #include "string.h"
-#include "ctype.h"
 
 int main()
 {
-    setScrollField(7, 46);
+    setScrollField(13, 46);
     printLine("================================================================================", 0, 0x0B);
-    printLine("                           neuer_user FTP Client v0.1                           ", 2, 0x0B);
-    printLine("--------------------------------------------------------------------------------", 4, 0x0B);
+    printLine("                           neuer_user FTP Client v0.2                           ", 1, 0x0B);
+    printLine("--------------------------------------------------------------------------------", 3, 0x0B);
+
+    iSetCursor(0, 4);
+    textColor(0x0F);
+
+    printf("Implemented functions:\n");
+    printf("-F1: Get file\t\t\t\t-F2: [Don't use]\n");
+    printf("-F3: Rename File\t\t\t-F4: Delete File\n");
+    //
+    printf("-F5: Create Directory\t\t\t-F6: Remove Directory\n");
+    printf("-F7: Change current directory\t\t-F8: Get current directory\n");
+    //
+    printf("-F9: List files/directories\n");
+    //
+    printf("-F12: Enter FTP-Command\n");
+    printf("--------------------------------------------------------------------------------\n\n");
 
     event_enable(true);
     char buffer[4096];
     EVENT_t ev = event_poll(buffer, 4096, EVENT_NONE);
-
-    iSetCursor(0, 7);
-    textColor(0x0F);
 
     char hostname[100];
     char user[100];
@@ -28,9 +39,14 @@ int main()
     printf("Passwort:\n");
     gets(pass);
 
-    uint32_t dataConnection = 0;
+    char command[200];
+    size_t waitingDataCommand = 0;
+    size_t renaming = 0;
+    size_t enterPasvMode = 0;
+
     IP_t dataIP;
     uint16_t dataPort;
+    uint32_t dataConnection = 0;
 
     IP_t IP = resolveIP(hostname);
     uint32_t control = tcp_connect(IP, 21);
@@ -47,6 +63,15 @@ int main()
             {
                 textColor(0x0A);
                 printf("\nESTABLISHED.\n\n");
+
+                tcpReceivedEventHeader_t* header = (void*)buffer;
+                char* data = (void*)(header+1);
+                data[header->length] = 0;
+                if (header->connectionID == dataConnection && waitingDataCommand)
+                {
+                    waitingDataCommand = 0;
+                    tcp_send(control, command, strlen(command));
+                }
                 break;
             }
             case EVENT_TCP_CLOSED:
@@ -57,7 +82,7 @@ int main()
                 if (header->connectionID == dataConnection)
                 {
                     textColor(0x07);
-                    printf("Closed data-connection.\n");
+                    printf("Closed dataConnection.\n");
                 }
                 break;
             }
@@ -68,7 +93,7 @@ int main()
                 data[header->length] = 0;
 
                 if (header->connectionID == dataConnection)
-                    textColor(0x0C);
+                    textColor(0x06);
                 else if (header->connectionID == control)
                     textColor(0x09);
                 printf("%s\n", data);
@@ -86,18 +111,18 @@ int main()
                         strcat(pStr,"\r\n");
                         tcp_send(control, pStr, strlen(pStr));
                     }
-                    else if (data[0] == '3' && data[1] == '3' && data[2] == '1')
-                    {
-                        char pStr[200];
-                        memset(pStr,0,200);
-                        strcat(pStr,"PASS ");
-                        strcat(pStr,pass);
-                        strcat(pStr,"\r\n");
-                        tcp_send(control, pStr, strlen(pStr));
-                    }
                     else if (data[0] == '2' && data[1] == '3' && data[2] == '0')
                     {
                         printf("Loggin successful.\n");
+                        tcp_send(control, "OPTS UTF8 ON\r\n", 14);
+                    }
+                    else if (data[0] == '2' && data[1] == '0' && data[2] == '0')
+                    {
+                        if (enterPasvMode)
+                        {
+                            enterPasvMode = 0;
+                            tcp_send(control, "PASV\r\n", 6);
+                        }
                     }
                     else if (data[0] == '2' && data[1] == '2' && data[2] == '6')
                     {
@@ -135,6 +160,23 @@ int main()
                         dataConnection = tcp_connect(dataIP, dataPort);
                         printf("\nConnected (ID = %u). Wait until connection is established... ", dataConnection);
                     }
+                    else if (data[0] == '3' && data[1] == '3' && data[2] == '1')
+                    {
+                        char pStr[200];
+                        memset(pStr,0,200);
+                        strcat(pStr,"PASS ");
+                        strcat(pStr,pass);
+                        strcat(pStr,"\r\n");
+                        tcp_send(control, pStr, strlen(pStr));
+                    }
+                    else if (data[0] == '3' && data[1] == '5' && data[2] == '0')
+                    {
+                        if (renaming)
+                        {
+                            renaming = 0;
+                            tcp_send(control, command, strlen(command));
+                        }
+                    }
                 }
                 else if (header->connectionID == dataConnection)
                 {
@@ -143,6 +185,7 @@ int main()
             }
             case EVENT_KEY_DOWN:
             {
+                textColor(0x0F);
                 KEY_t* key = (void*)buffer;
                 if (*key == KEY_ESC)
                 {
@@ -151,17 +194,115 @@ int main()
                     tcp_close(control);
                     return(0);
                 }
+                else if (*key == KEY_F1)
+                {
+                    memset(command,0,200);
+                    char filename[100];
+                    printf("Get file.\nEnter filename:\n");
+                    gets(filename);
+                    strcat(command,"RETR ");
+                    strcat(command,filename);
+                    strcat(command,"\r\n");
+                    waitingDataCommand = 1;
+                    enterPasvMode = 1;
+                    tcp_send(control, "TYPE A\r\n", 8);
+                }
+                else if (*key == KEY_F2)
+                {
+                    //STOR
+                }
+                else if (*key == KEY_F3)
+                {
+                    //Rename
+                    memset(command,0,200);
+                    char oldFilename[100];
+                    char newFilename[100];
+                    char tempCommand[200];
+                    printf("Rename.\nEnter current filename:\n");
+                    gets(oldFilename);
+                    printf("Enter new filename:\n");
+                    gets(newFilename);
+                    
+                    strcat(command,"RNTO ");
+                    strcat(command,newFilename);
+                    strcat(command,"\r\n");
+
+                    strcat(tempCommand,"RNFR ");
+                    strcat(tempCommand,oldFilename);
+                    strcat(tempCommand,"\r\n");
+                    renaming = 1;
+                    tcp_send(control, tempCommand, strlen(tempCommand));
+                }
+                else if (*key == KEY_F4)
+                {
+                    //Delete
+                    memset(command,0,200);
+                    char filename[100];
+                    printf("Delete file.\nEnter filename:\n");
+                    gets(filename);
+                    strcat(command,"DELE ");
+                    strcat(command,filename);
+                    strcat(command,"\r\n");
+                    tcp_send(control, command, strlen(command));
+                }
+                else if (*key == KEY_F5)
+                {
+                    //Create directory
+                    memset(command,0,200);
+                    char filename[100];
+                    printf("Make directory.\nEnter directory:\n");
+                    gets(filename);
+                    strcat(command,"MKD ");
+                    strcat(command,filename);
+                    strcat(command,"\r\n");
+                    tcp_send(control, command, strlen(command));
+                }
+                else if (*key == KEY_F6)
+                {
+                    //Remove directory
+                    memset(command,0,200);
+                    char filename[100];
+                    printf("Remove directory.\nEnter directory:\n");
+                    gets(filename);
+                    strcat(command,"RMD ");
+                    strcat(command,filename);
+                    strcat(command,"\r\n");
+                    tcp_send(control, command, strlen(command));
+                }
+                else if (*key == KEY_F7)
+                {
+                    //Change current directory
+                    memset(command,0,200);
+                    char dirname[100];
+                    printf("Change current directory.\nEnter directory(without initiating / !):\n");
+                    gets(dirname);
+                    strcat(command,"CWD ");
+                    strcat(command,dirname);
+                    strcat(command,"\r\n");
+                    tcp_send(control, command, strlen(command));
+                }
                 else if (*key == KEY_F8)
                 {
-                    textColor(0x0F);
+                    //Get current directory
+                    tcp_send(control, "PWD\r\n", 5);
+                }
+                else if (*key == KEY_F9)
+                {
+                    //List files/directories
+                    memset(command,0,200);
+                    strcat(command,"LIST -a\r\n");
+                    waitingDataCommand = 1;
+                    enterPasvMode = 1;
+                    printf("List files/directories.\n");
+                    tcp_send(control, "TYPE A\r\n", 8);
+                }
+                else if (*key == KEY_F12)
+                {
                     printf("Enter command:\n");
-                    char pStr[200];
-                    memset(pStr,0,200);
-                    char command[50];
+                    memset(command,0,200);
                     gets(command);
-                    strcat(pStr,command);
-                    strcat(pStr,"\r\n");
-                    tcp_send(control, pStr, strlen(pStr));
+                    strcat(command,"\r\n");
+                    tcp_send(control, command, strlen(command));
                 }
                 break;
             }
