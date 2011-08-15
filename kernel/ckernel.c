11f5f884fba94e3de1172541bf3bfda1d4901574
cooky451 <cooky451@31957799-4632-4615-9793-72db6d48bcac>
2011-08-15 18:19:21 +0000

    Revision angepasst.
    git-svn-id: svn://svn.code.sf.net/p/prettyos/code/trunk/Source@1174 31957799-4632-4615-9793-72db6d48bcac

--- kernel/ckernel.c
+++ kernel/ckernel.c
@@ -38,7 +38,7 @@
 #include "netprotocol/tcp.h"    // passive opened connection (LISTEN)
 
 
-const char* const version = "0.0.2.313 - Rev: 1172";
+const char* const version = "0.0.2.313 - Rev: 1174";
 
 // .bss
 extern uintptr_t _bss_start; // linker script
