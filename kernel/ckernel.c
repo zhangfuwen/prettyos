b81f012262bc4ab85e181e726463b3076cbce64c
cooky451 <cooky451@31957799-4632-4615-9793-72db6d48bcac>
2011-08-15 18:13:06 +0000

    version = "0.0.2.313 - Rev: 1172"
     - Neue Dateien in user_tools: dns.c dns.h dns_help.c dns_help.h
     - Neue Datei in other_userprogs: showdns.c
    
    dns.h bietet Funktionen zum parsen/erstellen von DNS Anfragen. 
    Momentan können nur header, questions und answers geparst werden, für Näheres siehe dns.c.
    dns_help.h bietet kleine Helferfunktionen, wie:
    getAddrByName();
    showDNSQuery();
    
    showdns.c: Ruft einfach showDNSQuery() auf; zum testen.
    
    git-svn-id: svn://svn.code.sf.net/p/prettyos/code/trunk/Source@1172 31957799-4632-4615-9793-72db6d48bcac

--- kernel/ckernel.c
+++ kernel/ckernel.c
@@ -38,7 +38,7 @@
 #include "netprotocol/tcp.h"    // passive opened connection (LISTEN)
 
 
-const char* const version = "0.0.2.312 - Rev: 1171";
+const char* const version = "0.0.2.313 - Rev: 1172";
 
 // .bss
 extern uintptr_t _bss_start; // linker script
