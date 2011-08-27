f5a13b65a8c16e67156db37739d81b0ae39b2d7e
ehenkes <ehenkes@31957799-4632-4615-9793-72db6d48bcac>
2011-08-27 20:10:26 +0000

    user-programme unter BSD-2 gestellt
    git-svn-id: svn://svn.code.sf.net/p/prettyos/code/trunk/Source@1218 31957799-4632-4615-9793-72db6d48bcac

--- user/other_userprogs/ftp.c
+++ user/other_userprogs/ftp.c
@@ -1,14 +1,20 @@
+/*
+*  license and disclaimer for the use of this source code as per statement below
+*  Lizenz und Haftungsausschluss für die Verwendung dieses Sourcecodes siehe unten
+*/
+
 #include "userlib.h"
 #include "stdlib.h"
 #include "stdio.h"
 #include "string.h"
 #include "dns_help.h"
 
+
 int main()
 {
     setScrollField(13, 46);
     printLine("================================================================================", 0, 0x0B);
-    printLine("                           neuer_user FTP Client v0.4",                            1, 0x0B);
+    printLine("                                FTP Client                                      ", 1, 0x0B);
     printLine("--------------------------------------------------------------------------------", 3, 0x0B);
 
     iSetCursor(0, 4);
@@ -405,3 +411,32 @@ int main()
     tcp_close(control);
     return(0);
 }
+
+
+/*
+* Copyright (c) 2011 The PrettyOS Project. All rights reserved.
+*
+* http://www.c-plusplus.de/forum/viewforum-var-f-is-62.html
+*
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice,
+*    this list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in the
+*    documentation and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+* PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+* OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+* OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+* ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
