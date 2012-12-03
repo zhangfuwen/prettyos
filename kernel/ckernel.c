a8b5514738c2e09a302ffb9919abe8f2a30851d1
pkeus <pkeus@31957799-4632-4615-9793-72db6d48bcac>
2012-12-03 17:38:10 +0000

    Version 0.0.4.11 - Revision 1396:
    - svn::mime-type und svn::executable für diverse Dateien gesetzt
    - PrettyOS.odt und Syscalls.odt aktualisiert. Erstere neu formatiert.
    
    git-svn-id: svn://svn.code.sf.net/p/prettyos/code/trunk/Source@1396 31957799-4632-4615-9793-72db6d48bcac

--- kernel/ckernel.c
+++ kernel/ckernel.c
@@ -40,7 +40,7 @@
 #include "netprotocol/tcp.h"    // tcp_showConnections, network_displayArpTables
 
 
-const char* const version = "0.0.4.11 - Rev: 1395";
+const char* const version = "0.0.4.11 - Rev: 1396";
 
 // .bss
 extern uintptr_t _bss_start; // Linker script
